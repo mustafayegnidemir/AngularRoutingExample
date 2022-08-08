b81a6ad173b3f31874bc21fe5069c718ade42510
mustafa <mustafayegnidemir1@gmail.com>
2022-08-08 14:13:44 +0300

    chlid(nested) routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,23 @@ import { CategoriesComponent } from './categories/categories.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ProductComponent } from './products/product/product.component';
+import { EditProductComponent } from './products/edit-product/edit-product.component';
+import { UserComponent } from './users/user/user.component';
 
 const appRoutes: Routes= [
 
-  {path:'', component: HomeComponent},   // localhost:4200
+  { path:'', component: HomeComponent},   // localhost:4200
   { path:'home', component: HomeComponent},
-  { path:'products', component:ProductsComponent},
-  { path:'products/:id', component:ProductComponent},
-  { path:'users', component:UsersComponent},
+  { path:'products', component:ProductsComponent, children: [
+    { path:':id', component:ProductComponent},
+    { path:':id/edit', component:EditProductComponent},
+
+    ]
+  },
+  { path:'users', component:UsersComponent, children: [
+    { path:':name', component:UserComponent}
+    ]
+  },
   { path:'**', component:NotfoundComponent}
 
 
@@ -33,7 +42,9 @@ const appRoutes: Routes= [
     UsersComponent,
     CategoriesComponent,
     NotfoundComponent,
-    ProductComponent
+    ProductComponent,
+    EditProductComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
